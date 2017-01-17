b9e72360ea9e3140b09b7d36dd3b9138266eb052
Hamza Nauman <hn9@rice.edu>
2017-01-17 17:58:35 -0600

    Added mailchimpAPI.py
    Added the python file containing the code I had written externally.

---
+++ mailchimpAPI.py
@@ -0,0 +1,54 @@
+import requests
+import json
+import sys
+import hashlib
+
+API_KEY = '827fbe9ae72d2a67fc8544054b71e680-us14'
+HOST = "https://us14.api.mailchimp.com/3.0/"
+
+def get_lists_info():
+    """
+    Returns a list of dictionaries, each containing information about each list of subscribers.
+    """
+    ENDPOINT = "lists"
+
+    url = "%s%s" % (HOST,ENDPOINT)
+
+    response = requests.get(url, auth=('', API_KEY))
+    return response.json()["lists"]
+
+# print (get_lists_info())
+
+def add_to_list(list_id,email,first_name,last_name):
+    """
+    Adds a subscriber to the specified list.
+    """
+    ENDPOINT = "lists/" + list_id + "/members" 
+
+    url = "%s%s" % (HOST,ENDPOINT)
+
+    params = {}
+    params["status"] = "subscribed"
+    params["email_address"] = email
+    params["merge_fields"] = {"FNAME":first_name,"LNAME":last_name}
+
+    response = requests.post(url,auth=('', API_KEY),data=json.dumps(params))
+    return response.json()
+
+# print (add_to_list("d0c0c7514d","hamzanauman@hotmail.com","Hamza","Nauman"))
+
+def convert_email_to_md5(email):
+    m = hashlib.md5()
+    m.update(email.lower().encode())
+    return (m.hexdigest())
+
+def delete_from_list(list_id,email):
+    subscriber_hash = convert_email_to_md5(email)
+    ENDPOINT = "lists/" + list_id + "/members/" + subscriber_hash
+
+    url = "%s%s" % (HOST,ENDPOINT)
+
+    response = requests.delete(url,auth=('',API_KEY))
+    return response
+
+print (delete_from_list("d0c0c7514d","hamzanauman@hotmail.com"))
